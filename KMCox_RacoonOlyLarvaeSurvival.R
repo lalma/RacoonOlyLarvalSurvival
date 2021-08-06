fc6ff088c377b9eb3a565a1101c7c73d228c6b9d
lalma <lalma@uw.edu>
2021-08-06 13:09:44 -0700

    survival stats

---
+++ KMCox_RacoonOlyLarvaeSurvival.R
@@ -0,0 +1,132 @@
+#kaplan meier survival
+
+#load libraries
+library(survival)
+library(survminer)
+library(ggplot2)
+library(rms)
+library(readxl)
+
+#upload datasheet 38,400 lines. once for each larvae
+OlyLarvaeKMforR <- read_excel("C:/Users/Lindsay/Dropbox/Raccoon/larvae/oyster/larvae survival stats/OlyLarvaeKMforR.xlsx")
+View(OlyLarvaeKMforR)
+
+#make surv object
+KMsurv = Surv(time = OlyLarvaeKMforR$day, OlyLarvaeKMforR$dead, type = "right")
+View(KMsurv)
+
+# fit KM model and plot without random effect of tank
+sf <- survfit(KMsurv ~ Treatment+Location, data = OlyLarvaeKMforR)
+summary(coxph(KMsurv ~ Treatment+Location, data = OlyLarvaeKMforR))
+
+# Graph the KMsurvival distribution
+#you can add pval = TRUE, but we know our p>0.0001
+ggsurvplot(sf, conf.int = 0.05)
+
+
+# another type of graph
+plot(sf, xlab="Larval Age", 
+     ylab="% Surviving", yscale=100, col=c("springgreen4", "royalblue3", "red","orange2","springgreen2","dodgerblue","indianred","gold1"),
+     main="% Larval Survival") 
+
+#Plot with breaktime by = # of days, palette=number of treatments listed colors
+fontsize<-20
+pCox <- ggsurvplot(sf, risk.table = FALSE, pval = FALSE, conf.int = TRUE,
+                   font.main = fontSize, font.x =  fontSize, font.y = fontSize, 
+                   font.tickslab = fontSize, font.legend = fontSize,
+                   palette = c("springgreen4", "royalblue3", "red","orange2","springgreen2","dodgerblue","indianred","gold1"), legend = "none"
+) +  xlab("Time (d)")
+pCox$plot
+
+#add a legend, dont need. probably make one not on R
+legend("topright" , c("CI20-14C","CI5-14C","DB-14C","PW-14C", "CI20-20C", "CI5-20C", "DB-20C", "PW-20C"),
+       fill=c("springgreen4", "royalblue3", "red","orange2","springgreen2","dodgerblue","indianred","gold1"))
+
+#another type of graph- will prob use this one
+ggsurvplot(sf, data=OlyLarvaeKMforR, conf.int=T,  risk.table=F, pval=F,legend=c("right"),
+           legend.labs=c("CI20-14C","CI5-14C","DB-14C","PW-14C", "CI20-20C", "CI5-20C", "DB-20C", "PW-20C"),legend.title="Treatment", 
+           palette =  c('darkgreen', 'blue4', 'darkred', 'darkgoldenrod', '#33CC66','steelblue','red','darkgoldenrod1'),   
+           risk.table.height=.25,xlab="Time (days)", size=0.7, break.time.by = 3,  ggtheme = theme_bw() +  theme(
+                   panel.grid.major.y = element_blank(),
+                   panel.grid.minor.y = element_blank(),  
+                   panel.grid.major.x = element_blank(),
+                   panel.grid.minor.x = element_blank()
+           ))
+
+
+#Test no parametric (logrank test) p<0.0001 chisq=4053
+survdiff(formula=Surv(day,dead)~Treatment +Location, data=OlyLarvaeKMforR)
+
+#p-value <0.0001
+surv_pvalue(sf) 
+
+#cox model sample code without interaction
+cox1<-coxph(KMsurv~TempAsFactor+Location, data=OlyLarvaeKMforR)
+summary(cox1)
+
+#cox model sample code with interaction, this is the model for the main analysis
+cox<-coxph(KMsurv~TempAsFactor+Location+TempAsFactor*Location, data=OlyLarvaeKMforR)
+cox##<here results!!!
+summary(cox)
+#38400 larvae, 25506 death, 12894 censored 
+#Looking at exp(coef)-- for example the Hazard Ratio of 20C is 0.586, this means that each additional
+#day is associated with a 0.586 fold increse in the hazard death when compared to 14C, and its 95% confidence
+#interval is (0.56,0.61). I think..Since this confidence interval is <1
+#it indicates a decrease in the hazard survival compared to 14C, and there is a significant association
+#between days and temperature (p<0.0001)
+anova(cox)
+cox.zph(cox)
+plot(cox.zph(cox)) 
+#A hazard ratio of 1 indicates no effect
+#A hazard ratio > 1 indicates an increase in the hazard as the covariate rises- It gets to death faster than the control
+#A hazard ratio < 1 indicates a decrease in the hazard as the covariate rises- it is slower go get to the event (death) than the cotnrol
+ggforest(cox, data=OlyLarvaeKMforR)
+
+#time-dependent covariate to fix proportional hazard assumption
+#A Cox model with time-dependent covariate would compare the risk of an event between transplant and nontransplant at each event time, but would re-evaluate
+#which risk group each person belonged in based on whether
+#they'd had a transplant by that time
+
+#model with temp only
+coxTemp<-coxph(KMsurv~TempAsFactor, data=OlyLarvaeKMforR)
+ggforest(coxTemp, data=OlyLarvaeKMforR)
+
+#model with locaiton only
+coxLocation<-coxph(KMsurv~Location, data=OlyLarvaeKMforR)
+ggforest(coxLocation, data=OlyLarvaeKMforR)
+
+#model with non categorial veraibles
+cox.number<-coxph(KMsurv~Temp+LocationNumber+Temp*LocationNumber, data=OlyLarvaeKMforR)
+cox.number
+summary(cox.number)
+ggforest(cox.number)
+
+############time varying coefficients
+
+#show time points in time at which an individual died
+cut.points <- unique(OlyLarvaeKMforR$DayDead[OlyLarvaeKMforR$Status == 1])
+
+#duplicate 4 line per individual, times 0-1, 1-7, 7-10, 10-13 =time0 and time
+SURV2 <- survSplit(data = OlyLarvaeKMforR, cut = cut.points, end = "DayDead", start = "DayDead0", event = "Status")
+View(SURV2)
+cut.points
+
+#run the cox model on orginal data- estimates one treatment male-21yrs
+model.1 <- coxph(Surv(DayDead, Status) ~ TempAsFactor+Location+TempAsFactor:LocationNumber, data = OlyLarvaeKMforR)
+model.1
+summary(model.1)
+#Schoenfeld's global test for the violation of proportional assumption-- Grambsch PM, Therneau TM. Proportional hazards tests
+# diagnostics based on weighted residuals. Biometrika 1994;81:515-26
+zph<-cox.zph(model.1)
+zph
+#results of cox.zph show there is significant deviation from the proportional hazards assumption for the variable
+covs <- data.frame(Location = "CI20", TempAsFactor = "14C")
+covs
+summary(survfit(model.1, newdata = covs, type = "aalen"))
+#output gives us probability of survival for CI20 14C for each day, i.e. a larvae has a .708 chance of survivind to day 4, but 0.163 chance of surviving to day 15
+cox.zph(model.1)
+#The result of zph shows that there is significant deviation from the proportional hazards assumption for all veriables
+ggforest(model.1, SURV2)
+
+
+
---
+++ fraility_RacoonOlyLarvaeSurvival.R
@@ -0,0 +1,157 @@
+#Load libraries.
+library(survminer)
+library(ggplot2)
+library(survival)
+library(frailtyEM)
+library(tidyverse)
+library(here)
+library(readxl)
+
+
+# Select dataset, import my dataset here, has the same headings as pauls dataset for simplicity
+OlyLarvaeKMforR <- read_excel("C:/Users/Lindsay/Dropbox/Raccoon/larvae/oyster/larvae survival stats/OlyLarvaeKMforR.xlsx")
+#  dataset is d
+d <- OlyLarvaeKMforR
+View(d)
+
+#make surv object
+surv = Surv(time = d$day, d$dead, type = "right")
+
+# coxph
+# fit model and plot without random effect
+sf <- survfit(surv ~ Treatment+Location, data = d)
+summary(coxph(surv ~ Treatment+Location, data = d))
+
+#basic KM plot
+ggsurvplot(sf, conf.int = TRUE)
+
+#Plot KM curves- will prob use this one for paper. Use code from R file RaccoonLarvaeSurvialCurves. contains all data
+#if we use the subset with 2000 lines, the confidence interval is much fatter
+ggsurvplot(sf, conf.int=T, risk.table=F, pval=F,legend=c("right"),
+           legend.labs=c("CI20-14C","CI5-14C","DB-14C","PW-14C", "CI20-20C", "CI5-20C", "DB-20C", "PW-20C"),legend.title="Treatment", 
+           palette =  c('darkgreen', 'blue4', 'darkred', 'darkgoldenrod', '#33CC66','steelblue','red','darkgoldenrod1'),   
+           xlab="Time (days)", size=0.7, break.time.by = 3,  ggtheme = theme_bw() +  theme(
+             panel.grid.major.y = element_blank(),
+             panel.grid.minor.y = element_blank(),  
+             panel.grid.major.x = element_blank(),
+             panel.grid.minor.x = element_blank()
+           ))
+
+#Best frailty distribution
+#find the frailty distribution with the maximium likelihood
+frailtyDist <- data.frame(name = c("gamma", "stablePos", "invGausian", "nonCenGamma"),
+                          dist = c("gamma", "stable", "pvf", "pvf"), m = c(0, 0, -0.5, 1))
+dLike <- NULL
+
+#here is where code runs a long time.Advice from Paul--
+#1. take it out of the loop so you are just running one distribution at a time. Then combine them at the end to find the best.
+#removed for loop
+#2. mess with the em_control options in the emfrail_control option of the emfrail function. It looks like there is a fast_fit option that may help and you can control the umber of interactions and tolerance of the algorithm.
+#emfrail_control=(fast_fit = TRUE)
+#3. Maybe try bootstrapping your data so you are working with a smaller data set for each fit. It would probably be a good idea to  just try a random subset of the data to start, just to test that the model is set up OK. That will also give you and idea of how long you might expect it to take to run your entire data set (if if takes x minutes to run 400 datapoints it will take y hours to to run 38,400 ...)
+#subsample 2000 lines at random, no replacement 
+
+### bootstrap small subsample, 2000 lines from 38400
+d <- OlyLarvaeKMforR
+d2000<-d[sample(nrow(d), 2000), ]
+#  dataset is d
+d <- d2000
+
+
+#added location as a second variable in addition to temp. cluster(group) tests random effect from replicates 
+
+#running first dist- name=nonCenGamma, dist=pvf, m=1, start 3:00 pm 7-14-21, found finished next morning 9:30 AM
+mLogLike <- emfrail(surv ~ Treatment + Location+ cluster(Group), 
+                    distribution = emfrail_dist(dist="pvf", pvfm = 1),
+                    emfrail_control=(fast_fit = TRUE),data = d)$loglik[2]
+dTemp <- data.frame(dist = "nonCenGamma", logLike = mLogLike)
+dLike <- rbind(dLike, dTemp)
+View(dLike) #dist	logLike = nonCenGamma
+#mLogLike=-9504.3757472149
+summary(mLogLike)
+
+#running second dist- name=invGausian, dist=pvf, m=-0.05, start 2:30 pm 7-15-21, was finished by morning 7-16-21 at 10am
+mLogLikeII <- emfrail(surv ~ Treatment + Location+ cluster(Group), 
+                    distribution = emfrail_dist(dist="pvf", pvfm = -0.05),
+                    emfrail_control=(fast_fit = TRUE),data = d)$loglik[2]
+dTempII <- data.frame(dist = "invGausian", logLike = mLogLikeII)
+View(dTempII)
+dLikeII <- NULL
+dLikeII <- rbind(dLikeII, dTempII)
+#mLogLikeII=-9504.3821743224
+summary(mLogLikeII)
+
+
+#running third dist- name=stablePos, dist=stable, m=0, start 4:00 pm 7-16-21 finish 7:30
+mLogLikeIII <- emfrail(surv ~ Treatment + Location+ cluster(Group), 
+                      distribution = emfrail_dist(dist="stable", pvfm = 0),
+                      emfrail_control=(fast_fit = TRUE),data = d)$loglik[2]
+dTempIII <- data.frame(dist = "stablePos", logLike = mLogLikeIII)
+View(dTempIII)
+dLikeIII <- NULL
+dLikeIII <- rbind(dLikeIII, dTempIII)
+#mLogLikeIII=-9504.87408829784
+summary(mLogLikeIII)
+
+
+#running fourth dist- name=gamma, dist=gamma, m=0, start 7:30pm pm 7-16-21 
+mLogLikeIIII <- emfrail(surv ~ Treatment + Location+ cluster(Group), 
+                       distribution = emfrail_dist(dist="gamma", pvfm = 0),
+                       emfrail_control=(fast_fit = TRUE),data = d)$loglik[2]
+dTempIIII <- data.frame(dist = "gamma", logLike = mLogLikeIIII)
+View(dTempIIII)
+dLikeIIII <- NULL
+dLikeIIII <- rbind(dLikeIIII, dTempIIII)
+#mLogLike=-9504.38193559969
+summary(mLogLikeIIII)
+
+#indentify the model frailty distribution with the maxium likelihood
+#since forloop was not used, created dataset with all distributions and their logLike values to enter into bestDist
+dLike <- read_excel("C:/Users/Lindsay/Dropbox/Raccoon/larvae/oyster/larvae survival stats/dLike.xlsx")
+bestDist <- dLike$dist[dLike$logLike == max(dLike$logLike)]
+#best fit was nonCenGamma
+
+# fit model with Group random effect using best fit frailty distribution
+#will take a while to run- start 4:15 7-21-21, finish by morning 7-21-21
+fme <- emfrail(surv ~ Treatment + Location+ cluster(Group), 
+               distribution = emfrail_dist(dist = frailtyDist$dist[frailtyDist$name == bestDist],
+                                           pvfm = frailtyDist$m[frailtyDist$name == bestDist] ),
+               data = d)
+summary(fme)
+#not sure how to read this summary...need help here.
+#are we looking for the p-values in the Fit summary to be <0.05?
+#Fit summary:
+#  Commenges-Andersen test for heterogeneity: p-val  0.335 
+#no-frailty Log-likelihood: -9504.875 
+#Log-likelihood: -9504.38 
+#LRT: 1/2 * pchisq(0.991), p-val 0.16
+
+#frailty plot - shows variation in frailty terms
+autoplot(fme, type = "frail")
+#new data of treatments for survival plots
+#dNew <- data.frame(Treatment = unique(d$Treatment)
+
+#made a new matrix called dNew, which has the unique treatments and locations. 
+#not sure if this is the correct format. need help here.
+#this data sheet has one row called Treatment and one row called Location
+dNew <- read_excel("C:/Users/Lindsay/Dropbox/Raccoon/larvae/oyster/larvae survival stats/dNew.xlsx")
+
+#predictions (survival probabilities) of treatmeents 
+#pred has 2 datasets, each are 12x15
+pred <- predict(fme, newdata = dNew, conf_int = "adjusted")
+
+#create a data frame of the prections for plotting all the treatment curves
+#changed from dTemp to dLike which has all of the distribution outcomes
+#need help here. not sure hot to integrate both response veriables into this loop
+dPlot <- NULL
+for(i in 1:length(pred)){
+  dLike <- pred[[i]] %>%
+    mutate(Treatment = unique(d$Treatment)[i])
+  dPlot <- rbind(dPlot, dTemp)
+}
+
+# plot curve
+ggplot(dPlot, aes(time, survival_m)) +
+  geom_step(aes(color = Treatment)) +
+  ylim(0,1) +
+  geom_blank()
